79cc84416b2c098835896f96ced0bfbaed68a203
Ori <uic3883@gmail.com>
2023-12-22 12:13:51 +0900

    Update GameManager.cs

--- Trickcal_CS/Assets/Scripts/GameManager.cs
+++ Trickcal_CS/Assets/Scripts/GameManager.cs
@@ -30,14 +30,4 @@ public class GameManager : MonoBehaviour
             pool.Get(1);
         }
     }
-
-    // 소환 데이터를 담당하는 클래스 선언
-    [System.Serializable] // 직렬화 (Serialization) : 개체를 저장 혹은 전송하기 위해 변환 -> 인스펙터창에서 보임
-    public class SpawnData
-    {
-        public float spawnTime; // 소환 시간
-        public int spriteType; // 몬스터 타입
-        public int health; // 체력
-        public float speed; // 속도
-    }
 }
